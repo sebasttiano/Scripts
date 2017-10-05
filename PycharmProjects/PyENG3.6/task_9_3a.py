4d1195abfcc09fa7c87c352ce5dee971d3d1737a
sebasttiano <graf.voronov@gmail.com>
2017-10-05 19:00:53 +0300

    Added tasks 9.3a and 9.3b

--- PycharmProjects/PyENG3.6/task_9_3a.py
+++ PycharmProjects/PyENG3.6/task_9_3a.py
@@ -1,29 +1,26 @@
 # -*- coding: utf-8 -*-
 
 '''
-Задание 9.3
+Задание 9.3a
 
-Создать функцию parse_cfg, которая ожидает как аргумент имя файла,
-в котором находится конфигурация устройства.
-
-Функция должна обрабатывать конфигурацию и возвращать IP-адреса и маски,
-которые настроены на интерфейсах, в виде списка кортежей:
-* первый элемент кортежа - IP-адрес
-* второй элемент кортежа - маска
+Переделать функцию parse_cfg из задания 9.3 таким образом, чтобы она возвращала словарь:
+* ключ: имя интерфейса
+* значение: кортеж с двумя строками:
+  * IP-адрес
+  * маска
 
 Например (взяты произвольные адреса):
-[('10.0.1.1', '255.255.255.0'), ('10.0.2.1', '255.255.255.0')]
+{'FastEthernet0/1':('10.0.1.1', '255.255.255.0'),
+ 'FastEthernet0/2':('10.0.2.1', '255.255.255.0')}
 
 Для получения такого результата, используйте регулярные выражения.
 
 Проверить работу функции на примере файла config_r1.txt.
 
-
 Обратите внимание, что в данном случае, можно не проверять корректность IP-адреса,
 диапазоны адресов и так далее, так как обрабатывается вывод команды, а не ввод пользователя.
 
 '''
-
 
 import re
 from sys import argv
@@ -31,12 +28,15 @@ from sys import argv
 filename = argv[1]
 
 def parse_cfg(filename):
+    result = {}
     source = '/home/svoronov/pyneng-examples-exercises/exercises/09_regex/{}'.format(filename)
     with open(source, 'r') as file:
         cfg = file.read()
-        regex = re.compile(' +ip address (?P<ip>\d+\.\d+\.\d+\.\d+) +(?P<mask>\d+\.\d+\.\d+\.\d+)')
-        return regex.findall(cfg)
-        #print (regex.findall(cfg))
+        regex = re.compile('interface (?P<inf>[LE]\S+)\n [^n].*?p address (?P<ip>\d+\.\d+\.\d+\.\d+) +(?P<mask>\d+\.\d+\.\d+\.\d+)', re.DOTALL)
+        match = regex.findall(cfg)
+        for i in match:
+            result[i[0]] = tuple(i[1:])
+        return result
 
 
 parse_cfg(filename)
