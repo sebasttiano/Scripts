899f888acf4fb6a22caddea2df78ee229d69b844
Sergey Voronov <graf.voronov@gmail.com>
2018-12-27 19:13:25 +0300

    update task_17_2.py

--- PycharmProjects/PyENG3.6/task_17/task_17_2.py
+++ PycharmProjects/PyENG3.6/task_17/task_17_2.py
@@ -36,19 +36,28 @@ def parse_file(file):
     content = ''
     with open(file, 'r') as f:
         for line in f:
-            content += line.rstrip()
+            content += line
     return content
+
+
+def get_payload(text):
+    list = re.split('Port ID', text)
+    return list[1]
 
 
 def parse_sh_cdp_neighbors(output):
     """:returns Example
     {'R4': {'Fa0/1': {'R5': 'Fa0/1'},
             'Fa0/2': {'R6': 'Fa0/0'}}}"""
-    hostname = re.search('(\w+)>', output).group(1)
-    #print(hostname)
-    local_int = re.findall(r'.*(Eth|Fa /d/+).*', output)
-    print(local_int)
-
+    result_dict = {}
+    hostname = re.search('(\S+)>', output).group(1)
+    print(hostname)
+    regex = r'(?P<rem_host>\S+)\s+(?P<local_int>\S+ \S+) +\d+.*\d+ +(?P<rem_int>\S+ \S+)'
+    #local_int = re.search(r'.*Port ID\s+(\w+)\s+((Eth|Fa) \d/\d).*', output)
+    preform = get_payload(output)
+    data = re.finditer(regex, preform)
+    for match in data:
+        print(match.groupdict())
 
 a = parse_file(FILE)
 parse_sh_cdp_neighbors(a)
